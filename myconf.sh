7e1bb95dfc75e6127e6c68af3f0985bba3e87b4d
James Anderson-Pole <james@pole.net.nz>
2022-07-07 21:57:05 +1200

    Update myconf.sh

--- myconf.sh
+++ myconf.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#
+# On macOS (Darwin) run the following command:
+#   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/jamespole/myconf/main/myconf.sh)"
+#
 
 if [ "$(uname)" == 'Darwin' ]; then
     echo 'INFO: Assuming macOS installation.'
