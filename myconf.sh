99c35f18dbcdaa7ee40e7828885f2d268eb8ff3b
James Anderson-Pole <james@pole.net.nz>
2022-07-07 22:07:28 +1200

    Update myconf.sh

--- myconf.sh
+++ myconf.sh
@@ -4,8 +4,14 @@
 #   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/jamespole/myconf/main/myconf.sh)"
 #
 
-if [ "$(uname)" == 'Darwin' ]; then
-    echo 'INFO: Assuming macOS installation.'
+if [ "$(uname)" = 'Darwin' ]; then
+    echo 'INFO: Assuming macOS.'
     brew update
     brew upgrade
+elif [ -f '/etc/debian_version' ]; then
+    echo 'INFO: Assuming Debian.'
+    sudo apt update
+    sudo apt upgrade
+else
+    echo 'ERROR: Could not detect operating system.'
 fi
