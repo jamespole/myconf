e7bd8994689d676f78acdf496a10e094b18aa4eb
James Anderson-Pole <james@pole.net.nz>
2022-07-07 22:16:58 +1200

    Update myconf.sh

--- myconf.sh
+++ myconf.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #
-# On macOS (Darwin) run the following command:
+# On systems with wget (e.g. Debian) run the following command:
+#  /bin/sh -c "$(wget -q -O - -- https://raw.githubusercontent.com/jamespole/myconf/main/myconf.sh)"
+#
+# On systems with curl (e.g. macOS) run the following command:
 #   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/jamespole/myconf/main/myconf.sh)"
 #
 
@@ -8,10 +11,16 @@ if [ "$(uname)" = 'Darwin' ]; then
     echo 'INFO: Assuming macOS.'
     brew update
     brew upgrade
+    brew install \
+        bash \
+        borgbackup \
+        ffmpeg \
+        shellcheck \
+        vim
 elif [ -f '/etc/debian_version' ]; then
     echo 'INFO: Assuming Debian.'
     sudo apt update
     sudo apt upgrade
 else
-    echo 'ERROR: Could not detect operating system.'
+    echo 'ERROR: Could not detect operating system.' && exit 1
 fi
