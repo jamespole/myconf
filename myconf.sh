90f2c3a2765a123e9a94ca832b2127b5de1d5020
James Anderson-Pole <james@pole.net.nz>
2022-07-08 15:38:27 +1200

    Update myconf.sh

--- myconf.sh
+++ myconf.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 #
+# myconf.sh: Portable configuration script.
+#
+# Copyright (c) 2022 James Anderson-Pole <james@pole.net.nz>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
+#
 # On systems with wget (e.g. Debian) run the following command:
 #  /bin/sh -c "$(wget -q -O - -- https://raw.githubusercontent.com/jamespole/myconf/main/myconf.sh)"
 #
